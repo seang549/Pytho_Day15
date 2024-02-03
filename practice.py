97389ddb31c023ee5907f55902342bd259591c73
Sean Guerrero <seanguerrero549@gmail.com>
2024-02-02 17:38:51 -0800

    finished coffee machine using OOP

---
+++ practice.py
@@ -0,0 +1,30 @@
+#import turtle
+
+# timmy = turtle.Turtle()
+
+#####OR######
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+#
+
+####USING TURTLE######
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DeepPink")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+#
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+print(table)
